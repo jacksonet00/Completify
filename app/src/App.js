1275ca51673d1278e537fdaf35e7e732d9a6793f
jacksonet00 <jacksoneliastaylor@gmail.com>
2020-08-05 23:16:13 -0400

    Implemented togglable auto sort

--- app/src/App.js
+++ app/src/App.js
@@ -3,6 +3,123 @@ import { v4 as uuidv4 } from 'uuid';
 import Task from './Task';
 import styled from 'styled-components';
 import './root.css';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const App = () => {
+	const [tasks, setTasks] = useState(
+		JSON.parse(localStorage.getItem('tasks')) || []
+	);
+	const [taskInput, setTaskInput] = useState('');
+	const [autoSort, setAutoSort] = useState(true);
+
+	useEffect(() => {
+		localStorage.setItem('tasks', JSON.stringify(tasks));
+	}, [tasks]);
+
+	useEffect(() => {
+		document.getElementById('add-btn').addEventListener('focus', (e) => {
+			window.setTimeout(() => e.target.blur(), 250);
+		});
+	}, []);
+
+	const handleAddTask = (e) => {
+		e.preventDefault();
+		if (taskInput !== '') {
+			const d = new Date();
+			setTasks([
+				...tasks,
+				{
+					name: taskInput,
+					completed: false,
+					id: uuidv4(),
+					time: d.getTime(),
+				},
+			]);
+			setTaskInput('');
+		}
+	};
+
+	const handleCompleteTask = (id) => {
+		let newTasks = [...tasks];
+		newTasks.forEach((t) => {
+			if (t.id === id) {
+				t.completed = !t.completed;
+			}
+		});
+		// TODO: Animate sorting.
+		// TODO: Sort subsections by date.
+		if (autoSort) {
+			newTasks.sort((a, b) => {
+				return a.completed === b.completed
+					? 0
+					: a.completed
+					? 1
+					: -1;
+			});
+		}
+		setTasks(newTasks);
+	};
+
+	const handleDeleteTask = (id) => {
+		const newTasks = tasks.filter((t) => t.id !== id);
+		setTasks(newTasks);
+	};
+
+	const toggleAutoSort = () => {
+		setAutoSort(!autoSort);
+		const newTasks = [...tasks];
+		if (!autoSort) {
+			newTasks.sort((a, b) => {
+				return a.completed === b.completed
+					? 0
+					: a.completed
+					? 1
+					: -1;
+			});
+		}
+		setTasks(newTasks);
+	};
+
+	return (
+		<Container>
+			<Title>Completify</Title>
+			<form onSubmit={(e) => handleAddTask(e)}>
+				<TextBox
+					type="text"
+					value={taskInput}
+					onChange={(e) => setTaskInput(e.target.value)}
+				/>
+				<AddButton type="submit" id="add-btn">
+					Add
+				</AddButton>
+			</form>
+			<div>
+				{tasks.map((t) => {
+					return (
+						<Task
+							key={t.id}
+							task={t}
+							onComplete={(id) => handleCompleteTask(id)}
+							onDelete={(id) => handleDeleteTask(id)}
+						/>
+					);
+				})}
+			</div>
+			<div>
+				<input
+					type="checkbox"
+					name="auto-sort"
+					id="auto-sort"
+					checked={autoSort}
+					onChange={() => toggleAutoSort()}
+				/>
+				<label htmlFor="auto-sort">Auto Sort</label>
+			</div>
+		</Container>
+	);
+};
+
+export default App;
 
 const Container = styled.div`
 	display: flex;
@@ -54,85 +171,13 @@ const AddButton = styled.button`
 		'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
 		'Helvetica Neue', sans-serif;
 
+	&:hover {
+		color: #25543b;
+	}
+
 	&:focus {
 		outline: 0;
 		box-shadow: inset 12px 12px 24px 0 rgba(0, 0, 0, 0.2),
 			inset -12px -12px 24px 0 rgba(255, 255, 255, 0.5);
 	}
 `;
-
-const App = () => {
-	const [tasks, setTasks] = useState(
-		JSON.parse(localStorage.getItem('tasks')) || []
-	);
-	const [taskInput, setTaskInput] = useState('');
-
-	useEffect(() => {
-		localStorage.setItem('tasks', JSON.stringify(tasks));
-	}, [tasks]);
-
-	const handleAddTask = (e) => {
-		e.preventDefault();
-		if (taskInput !== '') {
-			const d = new Date();
-			setTasks([
-				...tasks,
-				{
-					name: taskInput,
-					completed: false,
-					id: uuidv4(),
-					time: d.getTime(),
-				},
-			]);
-			setTaskInput('');
-		}
-	};
-
-	const handleCompleteTask = (id) => {
-		let newTasks = [...tasks];
-		newTasks.forEach((t) => {
-			if (t.id === id) {
-				t.completed = !t.completed;
-			}
-		});
-		// TODO: Animate sorting.
-		// TODO: Sort subsections by date.
-		newTasks.sort((a, b) => {
-			return a.completed === b.completed ? 0 : a.completed ? 1 : -1;
-		});
-		setTasks(newTasks);
-	};
-
-	const handleDeleteTask = (id) => {
-		const newTasks = tasks.filter((t) => t.id !== id);
-		setTasks(newTasks);
-	};
-
-	return (
-		<Container>
-			<Title>Completify</Title>
-			<form onSubmit={(e) => handleAddTask(e)}>
-				<TextBox
-					type="text"
-					value={taskInput}
-					onChange={(e) => setTaskInput(e.target.value)}
-				/>
-				<AddButton type="submit">Add</AddButton>
-			</form>
-			<div>
-				{tasks.map((t) => {
-					return (
-						<Task
-							key={t.id}
-							task={t}
-							onComplete={(id) => handleCompleteTask(id)}
-							onDelete={(id) => handleDeleteTask(id)}
-						/>
-					);
-				})}
-			</div>
-		</Container>
-	);
-};
-
-export default App;
